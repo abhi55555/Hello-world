4db7a5ebb69bd22ddb5fa83f8860db5142a474e7
abhi55555 <33001714+abhi55555@users.noreply.github.com>
2019-03-09 16:43:57 +0530

    Changed some code and fixed errors

--- arrays.py
+++ arrays.py
@@ -1,4 +1,4 @@
-'''cars = ['BMW','Porshe','Audi']
+cars = ['BMW', 'Porshe', 'Audi']
 for c in cars:
     print(c)
 print(len(cars))
@@ -16,28 +16,31 @@ print(cars)
 
 class newclass:
     te = 'teachers'
+
+
 q1 = newclass()
 print(q1.te)
-'''
+
+
 class person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
     def myfunc(self):
         print('Hello my name is ' + self.name)
 
-p1 = person("Ram",34)
+
+p1 = person("Ram", 34)
 p1.myfunc()
-#print(p1.name)
-#print(p1.age)
+# print(p1.name)
+# print(p1.age)
 p1.age = 100
-del p1
+#del p1
 print(p1.age)
 print('-----------')
 
-import numpy as np
+# import numpy as np
 
-a = np.array([12, 23], [45, 56])
-print(a)
-
-
+# a = np.array([12, 23], [45, 56])
+# print(a)
