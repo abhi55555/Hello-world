44d1c3dd0eae04e4c8e630709c4b8b4cb45aa4bf
abhi55555 <33001714+abhi55555@users.noreply.github.com>
2019-05-31 17:48:22 +0530

    older one

---
+++ older_mail_program.py
@@ -0,0 +1,21 @@
+
+import os
+import smtplib
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+RECEIVER = os.environ.get('RECEIVER')
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = 'Sample email'
+    body = 'This mail is send by running simple python program'
+
+    msg = f'Subject: {subject} \n\n {body}'
+
+    smtp.sendmail(EMAIL_ADDRESS, RECEIVER, msg)
